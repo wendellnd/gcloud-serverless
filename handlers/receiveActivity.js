54624b3aefa863a7841f8bbf828d02990652ef56
wendellnd <wendelldourado.wn@gmail.com>
2022-12-01 18:32:46 -0300

    fix: pubsub topic name

--- handlers/receiveActivity.js
+++ handlers/receiveActivity.js
@@ -1,7 +1,7 @@
 const pubsub = require("../helpers/pubsub");
 
 module.exports = async function receiveActivity(request, response) {
-  const result = await pubsub(request.body, "activity");
+  const result = await pubsub(request.body, "activities");
 
   console.log(request.body);
   response.send(JSON.stringify(result));
