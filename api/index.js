a9b7e60f118a632e7ef22fe776002af66e5a2654
wendellnd <wendelldourado.wn@gmail.com>
2022-12-01 19:19:30 -0300

    feat: add authorization

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
 const Koa = require("koa");
+const converter = require("basic-auth");
+
 const getAll = require("../repository/getAll");
 
 const application = new Koa();
 
 application.use(async (context) => {
+  const { name, pass } = converter.parse(context.request.headers.authorization);
+
+  if (process.env.USER !== name || process.env.PASS !== pass) {
+    context.status = 403;
+    context.body = {
+      message: "Access Denied",
+    };
+    return;
+  }
+
   context.status = 200;
   context.body = await getAll();
 });
